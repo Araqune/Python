717884a87b5a9448d64663fc5d4d0f79e4504207
Araqune <98ssergeys@gmail.com>
2024-07-22 22:01:45 +0300

    Строки и индексация строк
    Домашнее задание

---
+++ string_1.py
@@ -0,0 +1,12 @@
+string = 'Заземление' #Urban
+
+l = len(string)
+mid = l // 2
+
+print(string[0])
+print(string[-1])
+print(string[mid::])
+print(string[::-1])
+print(string[::2])
+
+print(len(string))
