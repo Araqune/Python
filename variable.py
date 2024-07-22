434826bdd15836044f696dbd329d45dec200903d
Araqune <98ssergeys@gmail.com>
2024-07-22 22:13:54 +0300

    Переменные
    домашнее задание

---
+++ variable.py
@@ -0,0 +1,5 @@
+homework_count = 12
+hours = 1.5
+course = 'Python'
+time = hours / homework_count
+print('Курс:', course + ', всего задач:', str(homework_count) + ', затрачено часов:', str(hours) + ', среднее время выполнения',time, 'часа.')
